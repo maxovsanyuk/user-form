73c2dd5fe749cdb7d8cced027a7310e160edb300
MaksOvs <maksovsanyuk@gmail.com>
2020-05-11 05:28:36 +0300

    Updates

---
+++ precache-manifest.1340afd7ae44990a218ca08d8a43da3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0578847292eb00108b5588f5d8e16b02",
+    "url": "/user-form/index.html"
+  },
+  {
+    "revision": "d4a640b15af564cf437c",
+    "url": "/user-form/static/js/2.1492561f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/user-form/static/js/2.1492561f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b894b3746a8728031f81",
+    "url": "/user-form/static/js/main.99c285be.chunk.js"
+  },
+  {
+    "revision": "cd13b305e6352fe1d47a",
+    "url": "/user-form/static/js/runtime-main.3693b027.js"
+  },
+  {
+    "revision": "ba9f19730fd128c60efcd68adf20369a",
+    "url": "/user-form/static/media/404.ba9f1973.gif"
+  }
+]);
